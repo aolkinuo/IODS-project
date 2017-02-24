5259e29ae129697ac699cf2a7bf13391b8a971b9
aolkinuo <anna-liina.olkinuora@helsinki.fi>
2017-02-24 23:04:22 +0200

    Harjoitus 5 tehty

--- Koodista poistettua.R
+++ Koodista poistettua.R
@@ -4,3 +4,15 @@ gender=learning2014$gender
 age=learning2014$Age
 attitude=learning2014$Attitude
 points=learning2014$Points
+
+library(plyr)
+
+rename(hd, c("Life expectancy"="life_exp", "Mean years of schooling"=
+               "m_school", "Expected years of schooling"="exp_school",
+             "GNI per capita"="GNI_perc"))
+
+rename(gii, c("Maternal mortality ratio"="mum_mort",
+              "Adolescent birth rate"="birth_rate", "Female and male population
+with at least secondary education"="fm_educ", "Female and
+male shares of parliamentary seats"="fm_parlseats", "Female and
+male labour force participation rates"="fm_labour"))
--- data/create_human.R
+++ data/create_human.R
@@ -1,41 +1,64 @@
+#Anna-Liina Olkinuora
+
+#Wrangling the human data
+
+#("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+#("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
-
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
-
-#En saanut aineistoja ladattua. En siis tiedä muuttujien
-#nimiä. Yritin kuitenkin kirjoittaa komennot ylös.
 
 str(hd)
 dim(hd)
 summary(hd)
-
 str(gii)
 dim(gii)
 summary(gii)
 
-library(plyr)
-
-rename(hd, c("Life expectancy"="life_exp", "Mean years of schooling"=
-"m_school", "Expected years of schooling"="exp_school",
-"GNI per capita"="GNI_perc"))
-
-rename(gii, c("Maternal mortality ratio"="mum_mort",
-"Adolescent birth rate"="birth_rate", "Female and male population
-with at least secondary education"="fm_educ", "Female and
-male shares of parliamentary seats"="fm_parlseats", "Female and
-male labour force participation rates"="fm_labour"))
-
-gii <- mutate(gii,  edu_ratio=edu2F / edu2M)
-
-gii <- mutate(gii, labor_ratio=labF / labM)
+new_names_hd <- c("HDI.Rank","Country","HDI","Life.Exp","Edu.Exp","Edu.Mean","GNI","GNI.Minus.Rank")
+new__names_gii <- c("GII.Rank", "Country", "GII","Mat.Mor","Ado.Birth","Parli.F","Edu2.F","Edu2.M","Labo.F","Labo.M")
+colnames(hd) <-new_names_hd 
+colnames(gii) <-new__names_gii  
+colnames(hd)
+colnames(gii)
 
 library(dplyr)
+gii <- mutate(gii,  Edu2.FM=(Edu2.F / Edu2.M))
+gii <- mutate(gii, Labo.FM=(Labo.F / Labo.M))
 
-join_by <- c("Countries")
-
+join_by <- c("Country")
 human <- inner_join(hd, gii, by = join_by, suffix=c(".hd", ".gii"))
 
-human<- select(human, one_of(join_by))
+setwd("C:/Users/Anna-Liina/Documents/IODS-project")
+write.table(human, file="data/human.txt", sep=",", row.names=TRUE)
 
-write.table(human, file="data/human.txt", sep=",")
+human <- mutate(human, as.numeric(GNI))
 
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human, one_of(keep))
+colnames(human)
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human)==TRUE)
+
+# look at the last 10 observations of human
+tail(human_, n=10)
+# define the last indice we want to keep
+last <- nrow(human_) - 7
+# choose everything until the last 7 observations
+human_ <- human_[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$Country
+# remove the Country variable
+human_ <- select(human_, -Country)
+
+dim(human_)
+human <- human_
+head(human)
+
+write.table(human, file="data/human.txt", sep=",", row.names= TRUE)
